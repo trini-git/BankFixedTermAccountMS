c173d509d8f7266b0d405d1f942272cfff36131a
U-USERSAD\ptrinida <ptrinida@LIM-JTZXMF2.usersad.everis.int>
2019-12-04 22:56:45 -0500

    Fifth Microservice

---
+++ src/main/java/com/bankfixedtermaccount/BankFixedTermAccountApplication.java
@@ -0,0 +1,13 @@
+package com.bankfixedtermaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankFixedTermAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankFixedTermAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankfixedtermaccount/BankFixedTermAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankfixedtermaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankFixedTermAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
